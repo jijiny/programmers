8793ea5818373790ba1c22deef5d1aceba1d38d8
jijiny <yoojin9325@naver.com>
2020-03-16 23:10:49 +0900

    조이스틱 (5,10,11 X) min/char+1

--- src/P_200318/Stick.java
+++ src/P_200318/Stick.java
@@ -3,27 +3,30 @@ package P_200318;
 public class Stick {
     public static int solution(String name) {
         int answer = 0;
-        int min = 65;
-        int max = 91;
         char[] nameChar = name.toCharArray();
 
         for(int i=0; i<nameChar.length; i++) {
-//            if('Z'-nameChar[i] > nameChar[i]-'A') {
+//            if('Z'-nameChar[i] +1 > nameChar[i]-'A') {
 //                System.out.println("거꾸로 : "+(nameChar[i] - 'A'));
 //                answer += nameChar[i]-'A';
-//            } else if('Z'-nameChar[i] < nameChar[i]-'A') {
-//                System.out.println("위로 : "+('Z' - nameChar[i]));
-//                answer += 'Z'-nameChar[i];
+//            } else if('Z'-nameChar[i] +1 < nameChar[i]-'A') {
+//                System.out.println("위로 : "+('Z' - nameChar[i] +1));
+//                answer += 'Z'-nameChar[i] +1;
 //            } else {
-//                System.out.println("같음 : " + ('Z' - nameChar[i]));
+//                System.out.println("같음 : " + ('Z' - nameChar[i] +1));
 //                answer += 'Z' - nameChar[i] +1;
 //            }
 
-            int cnt = Math.min(nameChar[i]-'A', 'Z'-nameChar[i]);
+            int cnt = Math.min(nameChar[i]-'A', 'Z'-nameChar[i]+1);
             answer += cnt;
         }
         for(int i=0; i<nameChar.length; i++) {
-
+            if(nameChar[i] == 'A') {
+                answer += name.length()-2;
+                break;
+            } else if(i==nameChar.length-1 ) {
+                answer += name.length()-1;
+            }
         }
 
         System.out.println(answer);
@@ -32,7 +35,7 @@ public class Stick {
 
     public static void main(String[] args) {
 //        String name = "JEROEN";
-        String name = "JAN";
+        String name = "JAZ";
 //        String name = "AABAZ";
 //        String name = "BAZZAAZ";
 //        String name = "AZZAAZ";
