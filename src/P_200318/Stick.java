a0cae5b464f943459b3ff10b097eb7e543ef88e2
jijiny <yoojin9325@naver.com>
2020-03-16 23:01:27 +0900

    조이스틱 (5,10,11 X) Math.min

--- src/P_200318/Stick.java
+++ src/P_200318/Stick.java
@@ -4,24 +4,26 @@ public class Stick {
     public static int solution(String name) {
         int answer = 0;
         int min = 65;
-        int max = 90;
+        int max = 91;
         char[] nameChar = name.toCharArray();
 
         for(int i=0; i<nameChar.length; i++) {
-            if('Z'-nameChar[i] +1 > nameChar[i]-'A') {
-                System.out.println("거꾸로 : "+(nameChar[i] - 'A'));
-                answer += nameChar[i]-'A';
-            } else if('Z'-nameChar[i] +1 < nameChar[i]-'A') {
-                System.out.println("위로 : "+('Z' - nameChar[i] +1));
-                answer += 'Z'-nameChar[i] +1;
-            } else {
-                System.out.println("같음 : " + ('Z' - nameChar[i] +1));
-                answer += 'Z' - nameChar[i] +1;
-            }
-        }
+//            if('Z'-nameChar[i] > nameChar[i]-'A') {
+//                System.out.println("거꾸로 : "+(nameChar[i] - 'A'));
+//                answer += nameChar[i]-'A';
+//            } else if('Z'-nameChar[i] < nameChar[i]-'A') {
+//                System.out.println("위로 : "+('Z' - nameChar[i]));
+//                answer += 'Z'-nameChar[i];
+//            } else {
+//                System.out.println("같음 : " + ('Z' - nameChar[i]));
+//                answer += 'Z' - nameChar[i] +1;
+//            }
 
+            int cnt = Math.min(nameChar[i]-'A', 'Z'-nameChar[i]);
+            answer += cnt;
+        }
         for(int i=0; i<nameChar.length; i++) {
-                
+
         }
 
         System.out.println(answer);
