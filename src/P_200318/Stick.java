006f57d40171eb659eaf5062fae9d4846505a059
jijiny <yoojin9325@naver.com>
2020-03-16 22:46:52 +0900

    조이스틱 (5,10,11 X) char[]

--- src/P_200318/Stick.java
+++ src/P_200318/Stick.java
@@ -4,32 +4,24 @@ public class Stick {
     public static int solution(String name) {
         int answer = 0;
         int min = 65;
-        int max = 91;
+        int max = 90;
         char[] nameChar = name.toCharArray();
-        int[] nameInt = new int[nameChar.length];
 
-        for(int i=0; i<nameInt.length; i++) {
-            nameInt[i] = (int) nameChar[i];
-        }
-        for(int i=0; i<nameInt.length; i++) {
-            if(max-nameInt[i] > nameInt[i]-min) {
-                System.out.println("거꾸로 : "+(nameInt[i] - min));
-                answer += nameInt[i]-min;
-            } else if(max-nameInt[i] < nameInt[i]-min) {
-                System.out.println("위로 : "+(max - nameInt[i]));
-                answer += max-nameInt[i];
-            } else {
-                System.out.println("같음 : " + (max - nameInt[i]));
-                answer += max - nameInt[i];
-            }
-        }
         for(int i=0; i<nameChar.length; i++) {
-            if(nameChar[i] == 'A') {
-                answer += name.length()-2;
-                break;
-            } else if(i==nameChar.length-1) {
-                answer += name.length()-1;
+            if('Z'-nameChar[i] +1 > nameChar[i]-'A') {
+                System.out.println("거꾸로 : "+(nameChar[i] - 'A'));
+                answer += nameChar[i]-'A';
+            } else if('Z'-nameChar[i] +1 < nameChar[i]-'A') {
+                System.out.println("위로 : "+('Z' - nameChar[i] +1));
+                answer += 'Z'-nameChar[i] +1;
+            } else {
+                System.out.println("같음 : " + ('Z' - nameChar[i] +1));
+                answer += 'Z' - nameChar[i] +1;
             }
+        }
+
+        for(int i=0; i<nameChar.length; i++) {
+                
         }
 
         System.out.println(answer);
@@ -37,9 +29,11 @@ public class Stick {
     }
 
     public static void main(String[] args) {
-//        String name = "JAZ";
 //        String name = "JEROEN";
         String name = "JAN";
+//        String name = "AABAZ";
+//        String name = "BAZZAAZ";
+//        String name = "AZZAAZ";
         solution(name);
     }
 }
