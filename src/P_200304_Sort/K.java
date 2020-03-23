cd9c255c1b5ca01727d9e7b3dec8e6275ac55373
jijiny <yoojin9325@naver.com>
2020-03-23 21:41:10 +0900

    K번째 수

--- src/P_200304_Sort/K.java
+++ src/P_200304_Sort/K.java
@@ -3,9 +3,7 @@ package P_200304_Sort;
 import java.util.*;
 
 public class K {
-    public static void main(String[] args) {
-        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
+    public static int[] solution(int[] array, int[][] commands) {
         int[] answer = new int [commands.length];
         int k=0;
 
@@ -19,9 +17,13 @@ public class K {
             k=0;
             answer[i] = command[commands[i][2]-1];
         }
-        for(int i=0; i<answer.length;i++) {
-            System.out.println(answer[i]);
-        }
 
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        solution(array, commands);
     }
 }
