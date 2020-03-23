5d5959db2d3ffc196037eec6e4a4e7893bac1319
jijiny <yoojin9325@naver.com>
2020-03-23 21:44:54 +0900

    정수 삼각형

--- src/P_200325_DynamicProgramming/IntegerTriangle.java
+++ src/P_200325_DynamicProgramming/IntegerTriangle.java
@@ -45,7 +45,6 @@ public class IntegerTriangle {
 
     public static void main(String[] args) {
         int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
-
         solution(triangle);
     }
 }
