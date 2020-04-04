44d1a8ee9e69a137b220e1da53ac4cf24a831887
jijiny <yoojin9325@naver.com>
2020-04-04 17:21:37 +0900

    network,,,하기 싫어,,,

---
+++ src/P_200408_DFS_BFS/Network.java
@@ -0,0 +1,17 @@
+package P_200408_DFS_BFS;
+
+public class Network {
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n=3;
+        int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
+//        int n=3;
+//        int[][] computers = {{1,1,0},{1,1,1},{0,1,1}};
+        solution(n, computers);
+    }
+}
